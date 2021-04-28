c65f070c2e24a77a6ac98596aea69214282de479
SailorMoon <yashamaru99@gmail.com>
2021-04-28 05:43:37 +0300

    Add project files.

---
+++ Homework 1/WeatherInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    public class WeatherInfo
+    {
+        public int Temperature { get; set; }
+        public DateTime Date { get; set; }
+
+        private List<WeatherInfo> info = new List<WeatherInfo>();
+    }
+}
